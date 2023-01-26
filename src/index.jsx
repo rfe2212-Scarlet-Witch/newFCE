844aa4e13bd551b75edb574f3426c94f823b7ef2
Jared Funaro <jaredfunaro@gmail.com>
2023-01-26 15:59:03 -0500

    added getProductInfo function and start setting up the server

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
-//import for overview
+import Overview from './Components/Overview.jsx';
 //import for QnA
 //import for reviews
 
 function App() {
   return (
     <div className="app">
-      <h1>To be used by overview component</h1>
+      <Overview/>
       <div>To be used by QnA component</div>
       <div>To be used by review component</div>
     </div>
