88695c3fac612041b1ffd6fa8424e6042abde2b8
formalmiguel <mig.alvarez92@gmail.com>
2023-01-26 14:20:07 -0600

    More changes to framework

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
+//import for overview
+//import for QnA
+//import for reviews
 
 function App() {
   return (
     <div className="app">
-      <h1>Checking one last time</h1>
-      <h2> Miguel checking in </h2>
-      <div>Jared Funaro</div>
-      <div>oheen</div>
+      <h1>To be used by overview component</h1>
+      <div>To be used by QnA component</div>
+      <div>To be used by review component</div>
     </div>
   );
 }
-
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
